e2b0dd3fd786b7c572b5e78f973714491bbb6155
Alex Subota <as.subota@gmail.com>
2015-07-28 01:51:20 +0300

    DiceRoller as module. Fixed dice setting function. Separate random function

--- dice_roller.js
+++ dice_roller.js
@@ -1,26 +1,47 @@
-var DiceRoller = function (dstr) {
-	this.init = function (str) {
+(function (global) {
+	var diceCount,
+		diceSides,
+		modifier;
+
+	function rand(min, max) {
+		// generate random integer from min (included) to max (included)
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	var DiceRoller = function DiceRoller(diceString) {
+		this.setDice(diceString);
+
+		return this;
+	};
+
+	DiceRoller.prototype.setDice = function (str) {
 		var dRexp = /(\d*)?d(\d+|F)([\+\-]\d*)?/,
 			parts = (str || '3d6').match(dRexp), // initialized as 3d6 by default
 			count = +parts[1],
 			mod = +parts[3] || 0,
-			sides = +parts[2];
-		this.diceCount = count || 1;
-		this.diceSides = sides || 3;
-		this.modifier = sides ? mod : mod - (count * 2);
-		
+			sides = parts[2],
+			isFudge = sides === 'F';
+
+		diceCount = count || 1;
+		diceSides = isFudge ? 3 : +sides;
+		modifier = isFudge ? mod - (diceCount * 2) : mod;
+
 		return this;
 	};
-	this.roll = function (str) {
-		if (str) this.init(str); // can be resetted to other dice
+
+	DiceRoller.prototype.roll = function (diceString) {
+		if (diceString) this.setDice(diceString); // can be resetted to other dice
 		var rndVal = 0;
-		for (var i = 0; i < this.diceCount; i++) {
-			rndVal += (Math.random() * this.diceSides + 1) >> 0
+		for (var i = 0; i < diceCount; i++) {
+			rndVal += rand(1, diceSides);
 		}
-		return rndVal + this.modifier;
+		return rndVal + modifier;
 	};
-	
-	this.init(dstr);
-	
-	return this;
-};
+
+	DiceRoller.prototype.formulaRoll = function (str) {
+		// TODO: implement formula roll
+	};
+
+	global.DiceRoller = DiceRoller;
+})(window);
+
